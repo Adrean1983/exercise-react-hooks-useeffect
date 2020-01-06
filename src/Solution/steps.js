f1029b1218c1c1d400744fa66b7af532f0032782
Adrean1983 <adrean_@hotmail.com>
2020-01-05 19:31:54 -0500

    react hooks + useEffect exercise

---
+++ src/Solution/steps.js
@@ -0,0 +1,13 @@
+// Taken from this video // Taken from this video https://www.youtube.com/watch?v=d9Pndaq9MJs&list=PLYyGnWAjIQwS0NcKqpNK8YaWhy0UU0VEh&index=13&t=163s
+
+const exercise1 = {
+  // import useState.
+  // Create text that says items in your cart with an array of cart items. Empty at first
+  // Create a button that adds items to the cart
+};
+
+const exercise2 = {
+  // create a state called scroll depth. Display the scroll depth on the page
+  // Write a function that gets scroll depth. determineScrollDepth. This function sets the state
+  // Use useEffect and an event listener to call the function when user scrolls. And display
+};
